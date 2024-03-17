18cb8dfb73ebe4d8b8258735c72a49e7ffbbe89c
Drakcodi <162724074+Drakcodi@users.noreply.github.com>
2024-03-17 15:26:37 +0530

    Add files via upload

---
+++ Th3-GPT/script.js
@@ -0,0 +1,12 @@
+const container = document.getElementById('container');
+const registerBtn = document.getElementById('register');
+const LoginBtn = document.getElementById('Login');
+
+registerBtn.addEventListener('click', () =>{
+    container.classList.add("active");
+});
+
+LoginBtn.addEventListener('click', () =>
+{
+    container.classList.remove("active");
+});
